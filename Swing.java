641c31a13ac08de67300d4c0c835177fd4dc9a8e
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-12-06 19:29:20 +0700

    update

---
+++ Swing.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+/**
+ * Swing
+ */
+public class Swing {
+
+    public static void main(String[] args) {
+        
+        // JFrame = a GUI window to add components to
+        
+        JFrame frame = new JFrame(); // creates a frame
+        frame.setTitle("Jrame title goes here"); // sets title of frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+        frame.setSize(420, 420); // sets the x-dimension and y-dimension of frame
+        frame.setVisible(true); // make frame visible
+    }
+}
