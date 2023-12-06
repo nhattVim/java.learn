042ee0f3692534dc22689ea264790640501d8360
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-12-06 19:31:20 +0700

    update

--- Swing.java
+++ Swing.java
@@ -11,6 +11,7 @@ public class Swing {
         
         JFrame frame = new JFrame(); // creates a frame
         frame.setTitle("Jrame title goes here"); // sets title of frame
+        frame.setVisible(false); // prevent frame from being resized
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
         frame.setSize(420, 420); // sets the x-dimension and y-dimension of frame
         frame.setVisible(true); // make frame visible
